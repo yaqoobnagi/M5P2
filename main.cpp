4f8aff3d0d1c83e6f7a3c4c3a4618b216db31e2c
yaqoobnagi <yaqoobnagi03@gmail.com>
2025-12-08 23:18:10 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,23 +5,63 @@
 using namespace std;
 
 int main() {
-    ProductionWorker pw1("Alice", "101", "01/01/2020", 1, 20.50);
-    ProductionWorker pw2("Bob", "102", "02/01/2021", 2, 22.75);
+    int choice;
 
-    ShiftSupervisor ss("Carol", "201", "03/01/2019", 60000, 5000);
+    do {
+        cout << "\n--- Employee Menu ---\n";
+        cout << "1. Create Production Worker\n";
+        cout << "2. Create Shift Supervisor\n";
+        cout << "3. Create Team Leader\n";
+        cout << "4. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
 
-    TeamLeader tl("Dave", "301", "04/01/2018", 1, 25.50, 1500, 40, 35);
+        if (choice == 1) {
+            string name, date;
+            int num, shift;
+            double rate;
 
-    cout << "\n--- Production Workers ---\n";
-    pw1.printProductionWorker();
-    cout << endl;
-    pw2.printProductionWorker();
+            bool valid = false;
+            ProductionWorker* pw = nullptr;
 
-    cout << "\n--- Shift Supervisor ---\n";
-    ss.printShiftSupervisor();
+            while (!valid) {
+                try {
+                    cout << "Name: ";
+                    cin >> name;
+                    cout << "Employee Number: ";
+                    cin >> num;
+                    cout << "Hire Date: ";
+                    cin >> date;
+                    cout << "Shift (1 or 2): ";
+                    cin >> shift;
+                    cout << "Hourly Pay Rate: ";
+                    cin >> rate;
 
-    cout << "\n--- Team Leader ---\n";
-    tl.printTeamLeader();
+                    pw = new ProductionWorker(name, num, date, shift, rate);
+                    valid = true;
+                }
+                catch (exception& e) {
+                    cout << "\n" << e.what() << endl;
+                    cout << "Please re-enter information.\n\n";
+                }
+            }
+
+            cout << "\n✔ Employee Created Successfully:\n";
+            pw->printProductionWorker();
+            delete pw;
+        }
+
+        else if (choice == 2) {
+            ShiftSupervisor ss("Carol", 201, "03/01/2019", 60000, 5000);
+            ss.printShiftSupervisor();
+        }
+
+        else if (choice == 3) {
+            TeamLeader tl("Dave", 301, "04/01/2018", 1, 25.50, 1500, 40, 35);
+            tl.printTeamLeader();
+        }
+
+    } while (choice != 4);
 
     return 0;
 }
