796059b6a4dffc0b2263898564c869f514360999
yaqoobnagi <yaqoobnagi03@gmail.com>
2025-12-08 23:17:27 -0500

    Update ProductionWorker.cpp

--- ProductionWorker.cpp
+++ ProductionWorker.cpp
@@ -1,18 +1,30 @@
 #include "ProductionWorker.h"
 
-ProductionWorker::ProductionWorker() : Employee() {
-    shift = 0;
+ProductionWorker::ProductionWorker()
+    : Employee() {
+    shift = 1;
     hourlyPayRate = 0.0;
 }
 
-ProductionWorker::ProductionWorker(string n, string num, string date, int s, double rate)
+ProductionWorker::ProductionWorker(string n, int num, string date, int s, double rate)
     : Employee(n, num, date) {
-    shift = s;
-    hourlyPayRate = rate;
+    setShift(s);
+    setHourlyPayRate(rate);
 }
 
-void ProductionWorker::setShift(int s) { shift = s; }
-void ProductionWorker::setHourlyPayRate(double rate) { hourlyPayRate = rate; }
+void ProductionWorker::setShift(int s) {
+    if (s != 1 && s != 2) {
+        throw InvalidShift();
+    }
+    shift = s;
+}
+
+void ProductionWorker::setHourlyPayRate(double rate) {
+    if (rate < 0) {
+        throw InvalidPayRate();
+    }
+    hourlyPayRate = rate;
+}
 
 int ProductionWorker::getShift() const { return shift; }
 double ProductionWorker::getHourlyPayRate() const { return hourlyPayRate; }
